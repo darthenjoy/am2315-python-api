bf34b260821e35b2e4f12cc1ed68bf9e4036f606
Joerg Ehrsam <joerg@ehrsam.de>
2013-09-15 20:02:17 +0200

    Version 1.3

--- examples/gettemp.py
+++ examples/gettemp.py
@@ -1,18 +1,13 @@
 from AM2315 import AM2315
-
+MAXTRIES = 5
 sensor=AM2315()
-#print(sensor.values())
-for i in range (0,5):
-  x=sensor.values()
-  try:
-    if x[0].find("err") > 0:
-       next
-  except:
-    try: 
-      if x[0] > 0:
-          print(x[0],x[1],x[2])
-          break 
-    except:
-      next
-if i > 0 :
-  print(i ,"retries needed")
+for i in range (0,MAXTRIES):
+  x = sensor.values()
+  if x[2] != 0:
+     next
+#  if (x[0] > 0 and x[1] != 0) or (x[0]>0 and i>MAXTRIES-1) :
+  print(x[0],x[1],x[2])
+  break
+#if i > 0 :
+#  print(i ,"retries needed")
+
